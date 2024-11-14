65973f6981d7970e88458fa7b0c03ad2ef9d3122
j8enni <125181887+j8enni@users.noreply.github.com>
2024-11-14 12:55:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -20,5 +20,5 @@ def search():
 def contact():
     return render_template('contact.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=3000)
